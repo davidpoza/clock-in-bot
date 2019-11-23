dd373b580f8094781b4733959baa37866058a427
David Poza Suárez <pozasuarez@gmail.com>
2019-11-23 19:42:09 +0100

    functional base

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Welcome'));
+
+bot.startPolling();
+
+bot.use(Telegraf.log());
+
+bot.command('start', ({ reply }) => {
+    reply('Olá');
+});
